fc685db65dbd4180c40e6b64d5f8dd49304fb264
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 23:34:25 -0500

    confconfiguracion inicial

---
+++ src/main/java/org/isai/api/comentario/publicaiones/api_rest_comentarios_publicaciones/ApiRestComentariosPublicacionesApplication.java
@@ -0,0 +1,13 @@
+package org.isai.api.comentario.publicaiones.api_rest_comentarios_publicaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestComentariosPublicacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestComentariosPublicacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/isai/api/comentario/publicaiones/api_rest_comentarios_publicaciones/ApiRestComentariosPublicacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isai.api.comentario.publicaiones.api_rest_comentarios_publicaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestComentariosPublicacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
