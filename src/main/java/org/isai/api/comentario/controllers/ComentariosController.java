84fa1dbe0b42f44bad7df16fe79bcd7bd375633c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 11:31:42 -0500

    implementacacion de un metodo para guardar Comentario (POST-MAPPING) en la clase controller

--- src/main/java/org/isai/api/comentario/controllers/ComentariosController.java
+++ src/main/java/org/isai/api/comentario/controllers/ComentariosController.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class ComentariosController {
 
@@ -14,8 +16,13 @@ public class ComentariosController {
     private ComentarioService service;
 
     @GetMapping("/publicaciones/{idPublicacion}/comentarios")
-    public Page<Comentario> obtenerComentariosPublicaciones(@PathVariable Long idPublicacion,Pageable pageable) {
+    public Page<Comentario> obtenerComentariosPublicaciones(@PathVariable Long idPublicacion, Pageable pageable) {
         return service.obtenerComentarios(idPublicacion, pageable);
+    }
+
+    @PostMapping("/publicaciones/{idPublicacion}/comentarios")
+    public Comentario guardarComentario(@PathVariable Long idPublicacion, @Valid @RequestBody Comentario comentario) {
+        return service.guardarComentario(comentario, idPublicacion);
     }
 
 }
