7ce65092e137e23ed6e0f4af6217e2f2899d3b53
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 00:56:22 -0500

    implementacion de los metodos para listar todas las publicaiones y ademas obtener una publiacacion por su ID

---
+++ src/main/java/org/isai/api/comentario/services/PublicacionService.java
@@ -0,0 +1,26 @@
+package org.isai.api.comentario.services;
+
+import org.isai.api.comentario.models.Publicacion;
+import org.isai.api.comentario.repositories.PublicacionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class PublicacionService {
+    @Autowired
+    private PublicacionRepository repository;
+
+    public Page<Publicacion> obtenerPublicaciones(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public Publicacion obtenerPublicacionPorId(Long id){
+        return repository
+        .findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("No se encontro Publicacion con ID: "+id));
+    }
+}
