5cac50bf3809c0d6a8cfef93d678cb15fb607866
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 10:32:25 -0500

    implementacion del metodo para listar las publicaciones (GET-MAPPING) Y (POST-MAPPING) para guardar una publicacion

---
+++ src/main/java/org/isai/api/comentario/controllers/PublicacionController.java
@@ -0,0 +1,30 @@
+package org.isai.api.comentario.controllers;
+
+import org.isai.api.comentario.models.Publicacion;
+import org.isai.api.comentario.services.PublicacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class PublicacionController {
+
+    @Autowired
+    private PublicacionService service;
+
+    @GetMapping("/publicaciones")
+    public Page<Publicacion> listarPublicaciones(Pageable pageable) {
+        return service.obtenerPublicaciones(pageable);
+    }
+
+    @PostMapping("/publicaciones")
+    public Publicacion guardarPublicacion(@Valid @RequestBody Publicacion publicacion) {
+        return service.guardarPublicacion(publicacion);
+    }
+}
