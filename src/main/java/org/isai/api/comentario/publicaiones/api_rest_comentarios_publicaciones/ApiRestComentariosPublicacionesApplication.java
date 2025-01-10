3a222fb025168bd79a0e53b8d492d85ab832fd85
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 23:59:02 -0500

    agregando el clase main la anotacion EnableJpaAuditing para darle uso a la clase auxiliar

--- src/main/java/org/isai/api/comentario/publicaiones/api_rest_comentarios_publicaciones/ApiRestComentariosPublicacionesApplication.java
+++ src/main/java/org/isai/api/comentario/publicaiones/api_rest_comentarios_publicaciones/ApiRestComentariosPublicacionesApplication.java
@@ -2,8 +2,10 @@ package org.isai.api.comentario.publicaiones.api_rest_comentarios_publicaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiRestComentariosPublicacionesApplication {
 
 	public static void main(String[] args) {
