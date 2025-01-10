226eb6f6d85d25439c35787bc10caa0eb94b7192
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 00:20:24 -0500

    definicion de metodos en la clase repositorio de Comentario

--- src/main/java/org/isai/api/comentario/repositories/ComentarioRepository.java
+++ src/main/java/org/isai/api/comentario/repositories/ComentarioRepository.java
@@ -1,11 +1,17 @@
 package org.isai.api.comentario.repositories;
 
+import java.util.Optional;
+
 import org.isai.api.comentario.models.Comentario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ComentarioRepository
         extends JpaRepository<Comentario, Long> {
+    Page<Comentario> findByPublicacion(Long idPublicacion, Pageable page);
 
+    Optional<Comentario> findByIdAndPublicacionId(Long comentarioID, Long publicacionID);
 }
